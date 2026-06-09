e9d7ef79eb759f29e781afcccc5bd01c1962d111
Nyisha Sangermano <nyishamarie.ai@gmail.com>
2026-06-09 15:02:29 -0400

    complete fizzbuzz

--- index.js
+++ index.js
@@ -227,5 +227,16 @@ export function moveWater(colander, bucket) {
  * fizzbuzz(15); // logs 1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, 11, fizz, 13, 14, fizzbuzz
  */
 export function fizzbuzz(n) {
-  // TODO
+  for (let i = 1; i <= n; i++) {
+    if(i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz")
+    } else if(i % 3 === 0) {
+      console.log("fizz");
+    } else if(i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i)
+    }
+  }
 }
+
